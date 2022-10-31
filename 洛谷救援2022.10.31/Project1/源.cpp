4c6eee8bcab40c89586fb8abf3d3eb51823ad2cc
2740172565fyl <fengyueleiandlym@outlook.com>
2022-10-31 19:48:05 +0800

    输出向上取整的用法

---
+++ 洛谷救援2022.10.31/Project1/源.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//    int n, i, c;
+//    double sum = 0, x, y, s;
+//    scanf("%d", &n);
+//    for (i = 1; i <= n; i++)
+//    {
+//        scanf("%lf %lf %d", &x, &y, &c);
+//        s = sqrt(x * x + y * y);//Î»ÖÃ
+//        sum += s / 25 + c * 1.5;
+//    }
+//    int b = sum;
+//    if (sum - b > 0)
+//    {
+//        b++;
+//    }
+//    printf("%d\n", b);
+//    return 0;
+//}
+#include<stdio.h>
+int main()
+{
+	int a = 1.6;
+	a++;
+	printf("%d", a);
+	return 0;
+}
